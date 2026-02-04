4a9c310884e1a1589608a9dc5a065fe1443c099b
Vinit Mehta <vinit2004mehtaa@gmail.com>
2026-02-04 17:06:27 +0530

    Fix data path in prepare_data.sh script

--- dataset/scannetv2/prepare_data.sh
+++ dataset/scannetv2/prepare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Copying Data
-python3 split_data.py --data_path ../../../dataset
+python3 split_data.py --data_path ../../../../dataset
 echo Preprocessing Data
 python3 prepare_data_inst.py
 python3 prepare_superpoint.py
